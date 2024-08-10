2dfc0c04d8596d726cd6e228fa0bf73535b768b0
denisolid <dkolisnichienko321@gmail.com>
2024-08-10 11:32:28 +0000

    ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,24 @@
+import { useSelector } from "react-redux";
+import {
+  selectContacts,
+  selectNameFilter,
+} from "../../redux/components/selectors";
 import Contact from ".//Contact/Contact";
 import styles from "./ContactList.module.css";
 
-const ContactList = ({ contacts, onDeleteContact }) => (
-  <ul className={styles.list}>
-    {contacts.map((contact) => (
-      <Contact
-        key={contact.id}
-        contact={contact}
-        onDeleteContact={onDeleteContact}
-      />
-    ))}
-  </ul>
-);
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+  // const filteredData = contacts.filter((item) =>
+  //   item.name.toLowerCase().includes(filter.toLowerCase())
+  // );
+  return (
+    <ul className={styles.list}>
+      {contacts.map((contact) => (
+        <Contact key={contact.id} contact={contact} />
+      ))}
+    </ul>
+  );
+};
 
 export default ContactList;
